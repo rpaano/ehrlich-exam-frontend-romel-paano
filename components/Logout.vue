17bad22ba2c7e85d2fcb593e74bd7587c35f9670
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 22:17:23 +0800

    Fix issue in change routes

--- components/Logout.vue
+++ components/Logout.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+const { logout } = useAuth()
+const router = useRouter()
+
 const buttonClasses: string[] = [
   'border', 'py-1', 'px-4', 'border-r-4',
   'border-b-4', 'border-black', 'hover:bg-gray-400'
 ]
 
 const userLogout = () => {
-  alert("here")
+  logout()
+  router.push({ path: "/" });
 }
-
 </script>
 
 <template>
