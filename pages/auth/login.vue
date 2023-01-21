9be97d4028987729d0d458cf05b73208ddf391be
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 17:17:26 +0800

    Remove unused components

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,44 +1,13 @@
-<template>
-  <div>
-    <form @submit.prevent="() => submit()">
-      <label for="email">Email</label>
-      <input name="email" type="email" v-model="emailCompute" />
-      <label for="password">Password</label>
-      <input name="password" type="password" v-model="passwordCompute"/>
-      <input type="submit" value="Submit">
-    </form>
-  </div>
-
-</template>
-
 <script setup lang="ts">
-const email = ref<string>("")
-const password = ref<string>("")
+const { query: routeQuery } = useRoute()
+
+const email = routeQuery.email
+const token = routeQuery.token
+const type = routeQuery.type
+
 const { login } = useAuth()
 
-const emailCompute = computed({
-  get() {
-    return email.value
-  },
-  set(value) {
-    // Add validation
-
-    email.value = value
-  }
-})
-
-const passwordCompute = computed({
-  get() {
-    return password.value
-  },
-  set(value) {
-    // Add validation
-
-    password.value = value
-  }
-})
-
-const submit = () => {
-  console.log(login({email: email.value, password: password.value}))
-}
 </script>
+
+<template>
+</template>
