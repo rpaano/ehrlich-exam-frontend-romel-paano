7412c483fd79fbc0c188b1c9ad05c1172e20d16f
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 09:40:28 +0800

    Add public runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [],
+  runtimeConfig: {
+    public: {
+      apiURL: 'http://api.ehrlich-exam-backend.test',
+      apiBase: '/api',
+    }
+  }
 })
