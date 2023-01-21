c3f30fd4e71db7216040b17881dca7dceb40b5cb
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 10:36:31 +0800

    Add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  css: [
+    `@/assets/css/tailwind.css`,
+  ],
+  tailwindcss: {
+    cssPath: `~/assets/css/tailwind.css`,
+    configPath: `./tailwind.config.ts`,
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
   runtimeConfig: {
     public: {
       apiURL: 'http://api.ehrlich-exam-backend.test',
       apiBase: '/api',
     }
-  }
+  },
+
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'false',
+  theme: {},
+  plugins: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+}
