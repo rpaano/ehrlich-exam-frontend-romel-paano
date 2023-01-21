4d69748f14cec51346e946937e563f5802d5bd95
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-22 00:00:10 +0800

    Add user name and link for user

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    dashboard
-  </div>
+  <section class="flex">
+    <div class="sm:mx-auto w-[500px] h-[200px] mt-24 mx-10">
+      <Paragraph :text="user.name" class=" text-center"/>
+      <div class="flex mt-4">
+        <NuxtLink target="_blank" :href="user.github_link" class="text-center w-full" >{{ user.github_link }}</NuxtLink>
+      </div>
+      <div>
+        <input type="text" >
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-
 import auth from '~~/middleware/auth';
 
+const user = useUser()
 
 definePageMeta({
   middleware: [auth]
