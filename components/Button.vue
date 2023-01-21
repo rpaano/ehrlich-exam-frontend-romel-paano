566a2916dc466c400d43ba6e62eecf346a6c1bd9
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 11:15:10 +0800

    Create Button component

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class=" capitalize" :class="classNames">
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+
+interface ButtonProps {
+  text: string
+  classNames?: string[]
+}
+
+const { text, classNames } = defineProps<ButtonProps>()
+</script>
