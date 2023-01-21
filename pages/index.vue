7a352a78f17d4dcfeedeef5b0a5d5eaa87be1d8a
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2023-01-21 16:02:31 +0800

    Update the layout of the code

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
+const { apiURL, githubLoginRedirect } = useRuntimeConfig()
+
 const welcomeMessage = ref<string>("Welcome to the weather forecast web application. Please use your Github account to user the application view the weather in your city.")
-const buttonClasses: string[] = ['mt-12', 'border', 'py-2', 'px-4', 'border-r-4', 'border-b-4', 'border-black', 'hover:bg-gray-400', "sm:py-4"]
-const paragrahplasses: string[] = ["sm:py-4"]
-
-
-const gotoLogin = () => {
-  navigateTo('auth/login')
-}
+const buttonClasses: string[] = ['border', 'py-2', 'px-4', 'border-r-4', 'border-b-4', 'border-black', 'hover:bg-gray-400', "sm:py-4"]
+const paragrahplasses: string[] = ["sm:py-4", "mb-12"]
 
 </script>
 
 <template>
-  <section class="flex justify-cener ">
+  <section class="flex justify-cener">
     <div class="sm:mx-auto w-[500px] h-[200px] mt-24 mx-10">
       <Paragraph :text="welcomeMessage" :class-name="paragrahplasses" />
-      <Button text="Login" @click="() => gotoLogin()" :class-names="buttonClasses" />
+      <NuxtLink :href="`${apiURL}${githubLoginRedirect}`" :class="buttonClasses" >Login</NuxtLink>
     </div>
   </section>
 </template>
